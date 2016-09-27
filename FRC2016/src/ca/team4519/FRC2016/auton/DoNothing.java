d39a6f4f1453a120f55f62bc30fbc16eb0d1696b
CAdamz <connordgadams@gmail.com>
2016-09-27 14:21:50 -0400

    Cleaned and commented

---
+++ FRC2016/src/ca/team4519/FRC2016/auton/DoNothing.java
@@ -0,0 +1,29 @@
+package ca.team4519.FRC2016.auton;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *This Auton mode literally does nothing
+ */
+public class DoNothing extends Command {
+
+    public DoNothing() {
+
+    }
+
+    protected void initialize() {
+    }
+
+    protected void execute() {
+    }
+
+    protected boolean isFinished() {
+        return true;
+    }
+
+    protected void end() {
+    }
+
+    protected void interrupted() {
+    }
+}
